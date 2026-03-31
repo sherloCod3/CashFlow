5393ab695d6286dc8d158bbe77615b0c8f7e0164
Alexandre Cavalari <alexandre.cavalari@outlook.com>
2026-03-31 16:54:37 -0300

    test(expenses/register): add validation tests
    - Add tests for empty title
    - Add test for future date
    - Add test for invalid payment type
    - Add tests for invalid amount

--- CashFlow.Application/UsesCases/Expenses/Register/RegisterExpenseValidator.cs
+++ CashFlow.Application/UsesCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -1,6 +1,5 @@
 using CashFlow.Communication.Requests;
 using CashFlow.Exception;
-
 using FluentValidation;
 
 namespace CashFlow.Application.UsesCases.Expenses.Register
@@ -9,10 +8,18 @@ namespace CashFlow.Application.UsesCases.Expenses.Register
 	{
 		public RegisterExpenseValidator()
 		{
-			RuleFor(expense => expense.Title).NotEmpty().WithMessage(ResourceErrorMessages.TITLE_REQUIRED);
-			RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
-			RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessages.EXPENSES_CANNOT_BE_IN_FUTURE);
-			RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage(ResourceErrorMessages.PAYMENT_TYPE_INVALID);
+			RuleFor(expense => expense.Title)
+				.NotEmpty()
+				.WithMessage(ResourceErrorMessages.TITLE_REQUIRED);
+			RuleFor(expense => expense.Amount)
+				.GreaterThan(0)
+				.WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
+			RuleFor(expense => expense.Date)
+				.LessThanOrEqualTo(DateTime.UtcNow)
+				.WithMessage(ResourceErrorMessages.EXPENSES_CANNOT_BE_IN_FUTURE);
+			RuleFor(expense => expense.PaymentType)
+				.IsInEnum()
+				.WithMessage(ResourceErrorMessages.PAYMENT_TYPE_INVALID);
 		}
 	}
 }
